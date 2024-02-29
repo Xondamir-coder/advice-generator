198c1ee329222f04c05c3e3c2d067de37cf9dee7
Xondamir Nazrullayev <xondamirnazrullayev@mail.ru>
2024-02-29 22:59:14 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,25 @@
+const modalText = document.querySelector('.modal__text');
+const modalNumber = document.querySelector('.modal__number');
+const modalButton = document.querySelector('.modal__button');
+
+const getAdvice = async () => {
+	const URL = 'https://api.adviceslip.com/advice';
+	try {
+		const res = await fetch(URL);
+		const data = await res.json();
+		const advice = data.slip.advice;
+		const id = data.slip.id;
+
+		modalText.textContent = `“${advice}”`;
+		modalNumber.textContent = `advice #${id}`;
+
+		[modalText, modalNumber].forEach(el => el.classList.remove('blurred'));
+	} catch (error) {
+		console.error(error);
+	}
+};
+getAdvice();
+
+modalButton.addEventListener('click', () => {
+	getAdvice();
+});
